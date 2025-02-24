6ae0b06b0d466b34d8378badd65b6ad08c2ce2a6
Gustavo Araujo <121242417+gustavofda@users.noreply.github.com>
2025-02-23 21:58:20 -0300

    fix: Remover asserção de não-nulo e adicionar verificação segura para o elemento root

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import { CssBaseline, ThemeProvider } from '@mui/material'
-import theme from './theme.ts'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import theme from './theme.ts';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>,
-)
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </React.StrictMode>
+  );
+} else {
+  console.error('Elemento root não encontrado');
+}
