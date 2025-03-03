eef34c8ab5db7959ac598c9513cb94ef9e17db6e
Gustavo Araujo <121242417+gustavofda@users.noreply.github.com>
2025-03-02 23:36:38 -0300

    feat: img avatar

--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -1,5 +1,5 @@
 import { Box, Container, Grid, Typography, styled } from "@mui/material"
-import Avatar from "../../../../assets/images/avatar.jpg"
+import Avatar from "../../../../assets/images/Avatar.jpg"
 import DownloadIcon from '@mui/icons-material/Download';
 import MailOutlineIcon from '@mui/icons-material/MailOutline';
 import StyledButton from "../../../../components/StyledButton/StyledButton";
