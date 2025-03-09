732e88d46876e3fa955b2d983518078ad99510c4
Gustavo Araujo <121242417+gustavofda@users.noreply.github.com>
2025-03-09 17:50:32 -0300

    feat(projects): adiciona imagens e descrições detalhadas dos projetos

--- src/pages/Home/sections/Projects/Projects.tsx
+++ src/pages/Home/sections/Projects/Projects.tsx
@@ -1,15 +1,21 @@
-import { Typography, styled, Grid } from '@mui/material';
+import { Typography, styled, Grid, Link } from '@mui/material';
 import { Element } from 'react-scroll';
 import { motion } from 'framer-motion';
 import { useEffect, useState, useRef } from 'react';
 
 import dexterImage from '../../../../assets/Imageproject/dexter.png';
+import convertCoin from '../../../../assets/Imageproject/convermoedas.png';
+import utopiaImage from '../../../../assets/Imageproject/utopia.png'
+import metasImage from '../../../../assets/Imageproject/metas.png'
 
 const projects = [
-  { id: 1, title: 'Projeto Dexter', description: 'Descrição do projeto Dexter', image: dexterImage },
-  { id: 2, title: 'Projeto 2', description: 'Descrição do Projeto 2', image: dexterImage },
-  { id: 3, title: 'Projeto 3', description: 'Descrição do Projeto 3', image: dexterImage },
-  { id: 4, title: 'Projeto 4', description: 'Descrição do Projeto 4', image: dexterImage },
+  { id: 1, title: 'Projeto Dexter', description: 'Projeto Dexter é um site desenvolvido em React com Tailwind CSS, dedicado à série Dexter. O projeto apresenta um menu com diferentes componentes, cada um focado em uma série específica, com design responsivo e uma interface moderna. O objetivo é aprimorar habilidades em desenvolvimento frontend, organização de componentes e estilização avançada com Tailwind.', image: dexterImage, link: 'https://github.com/seu-usuario/projeto-dexter' },
+
+  { id: 2, title: 'ConvCoin: Conversor de Moedas', description: 'ConvCoin é um app de conversão de moedas que utiliza uma API de câmbio em tempo real para fornecer taxas sempre atualizadas. Com uma interface intuitiva, ele facilita a conversão entre moedas estrangeiras e o real, sendo ideal para viajantes e investidores. Além disso, o app garante rapidez e precisão, permitindo cálculos instantâneos para melhor planejamento financeiro.', image: convertCoin, link: 'https://github.com/seu-usuario/convcoin' },
+
+  { id: 3, title: 'Travis Scott: Utopia', description: 'Este projeto é uma landing page dedicada ao álbum "Utopia" do rapper Travis Scott. A página inclui uma seção animada de boas-vindas, um reprodutor de música integrado e um navbar com links relevantes. O objetivo é criar uma página visualmente atraente e funcional para promover o álbum e a turnê do artista.', image: utopiaImage, link: 'https://github.com/seu-usuario/utopia' },
+
+  { id: 4, title: 'App: Controle de Metas', description: 'App de Controle de Metas é uma aplicação em TypeScript, React e Node.js para criação e acompanhamento de metas semanais. O usuário define a frequência desejada via formulário, e as metas são enviadas para a API via POST. As metas pendentes são exibidas em uma lista, onde podem ser marcadas como concluídas, atualizando o progresso automaticamente.', image: metasImage, link: 'https://github.com/seu-usuario/controle-de-metas' },
 ];
 
 const Projects: React.FC = () => {
@@ -88,8 +94,11 @@ const Projects: React.FC = () => {
               >
                 <ProjectGrid>
                   <ProjectImage src={project.image} alt={project.title} />
-                  <Typography variant="h5">{project.title}</Typography>
+                  <Typography variant="h5"><strong>{project.title}</strong></Typography>
                   <Typography variant="body1">{project.description}</Typography>
+                  <Link href={project.link} target="_blank" rel="noopener" sx={{ marginTop: 2, color: 'lightblue' }}>
+                    Leia mais
+                  </Link>
                 </ProjectGrid>
               </motion.div>
             </Grid>
