15f319186a774b06ad0534946056a4f25b6f3852
Gustavo Araujo <121242417+gustavofda@users.noreply.github.com>
2025-03-09 21:29:24 -0300

    feat: adicionado a  descrição dos projetos, e link correto de cada 'leia mais'

--- src/pages/Home/sections/Projects/Projects.tsx
+++ src/pages/Home/sections/Projects/Projects.tsx
@@ -9,10 +9,13 @@ import utopiaImage from '../../../../assets/Imageproject/utopia.png';
 import metasImage from '../../../../assets/Imageproject/metas.png';
 
 const projects = [
-  { id: 1, title: 'Projeto Dexter', description: 'Projeto dedicado à série Dexter...', image: dexterImage, link: 'https://github.com/seu-usuario/projeto-dexter' },
-  { id: 2, title: 'ConvCoin: Conversor de Moedas', description: 'App de conversão de moedas...', image: convertCoin, link: 'https://github.com/seu-usuario/convcoin' },
-  { id: 3, title: 'Travis Scott: Utopia', description: 'Landing page sobre o álbum "Utopia"...', image: utopiaImage, link: 'https://github.com/seu-usuario/utopia' },
-  { id: 4, title: 'App: Controle de Metas', description: 'Aplicação para acompanhar metas...', image: metasImage, link: 'https://github.com/seu-usuario/controle-de-metas' },
+  { id: 1, title: 'Dexter: The Bay Harbor Site', description: 'The Bay Harbor Site é um site desenvolvido em React com Tailwind CSS, dedicado à série Dexter. O projeto apresenta um menu com diferentes componentes, cada um focado em uma série específica, com design responsivo e uma interface moderna. O objetivo é aprimorar habilidades em desenvolvimento frontend, organização de componentes e estilização avançada com Tailwind.', image: dexterImage, link: 'https://github.com/gustavofda/TheBayHarbor-Site' },
+
+  { id: 2, title: 'ConvCoin: Conversor de Moedas', description: 'ConvCoin é um app de conversão de moedas que utiliza uma API de câmbio em tempo real para fornecer taxas sempre atualizadas. Com uma interface intuitiva, ele facilita a conversão entre moedas estrangeiras e o real, sendo ideal para viajantes e investidores. Além disso, o app garante rapidez e precisão, permitindo cálculos instantâneos para melhor planejamento financeiro.', image: convertCoin, link: 'https://github.com/gustavofda/ConvCoin' },
+
+  { id: 3, title: 'Travis Scott: Utopia', description: 'Este projeto é uma landing page dedicada ao álbum "Utopia" do rapper Travis Scott. A página inclui uma seção animada de boas-vindas, um reprodutor de música integrado e um navbar com links relevantes. O objetivo é criar uma página visualmente atraente e funcional para promover o álbum e a turnê do artista.', image: utopiaImage, link: 'https://github.com/gustavofda/TOUR-CIRCUS-MAXIMUS' },
+
+  { id: 4, title: 'App: Controle de Metas', description: 'App de Controle de Metas é uma aplicação em TypeScript, React e Node.js para criação e acompanhamento de metas semanais. O usuário define a frequência desejada via formulário, e as metas são enviadas para a API via POST. As metas pendentes são exibidas em uma lista, onde podem ser marcadas como concluídas, atualizando o progresso automaticamente. ', image: metasImage, link: 'https://github.com/seu-usuario/controle-de-metas' },
 ];
 
 const Projects = () => {
@@ -72,7 +75,7 @@ const Projects = () => {
   return (
     <Element name="projects-section">
       <StyledProjects ref={projectsRef}>
-        <Typography variant="h2" align="center" sx={{ marginBottom: 4, color: 'white' }}>
+        <Typography variant="h1" align="center" sx={{ marginBottom: 4, color: 'white' }}>
           Projects
         </Typography>
         <Grid container spacing={4}>
