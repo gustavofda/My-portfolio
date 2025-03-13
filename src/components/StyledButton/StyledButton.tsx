7fad927b0ba9430fec29a888c2826cef2513efeb
Gustavo Araujo <121242417+gustavofda@users.noreply.github.com>
2025-03-13 15:46:50 -0300

    fix: bug para fazer download de arquivos

--- src/components/StyledButton/StyledButton.tsx
+++ src/components/StyledButton/StyledButton.tsx
@@ -1,36 +1,36 @@
-import { styled } from "@mui/material";
-import type { ReactNode, ElementType } from "react";
+import { Button } from '@mui/material';
+import type { ButtonProps } from '@mui/material';
+import { styled } from '@mui/system';
 
-interface StyledButtonProps {
-  children: ReactNode;
-  component?: ElementType;
+interface StyledButtonProps extends ButtonProps {
+  component?: React.ElementType;
   href?: string;
-  download?: boolean;
+  download?: string;
 }
 
-const StyledButton: React.FC<StyledButtonProps> = ({ children, component = "button", href, download }) => {
-  const StyledButtonComponent = styled(component)(({ theme }) => ({
-    backgroundColor: "transparent",
-    border: `1px solid ${theme.palette.primary.contrastText}`,
-    borderRadius: "3px",
-    padding: "5px 15px",
-    width: "100%",
-    color: theme.palette.primary.contrastText,
-    display: "inline-flex",
-    alignItems: "center",
-    justifyContent: "center",
-    gap: "10px",
-    textDecoration: "none", // Adiciona esta linha para remover o sublinhado
-    '&:hover': {
-      backgroundColor: theme.palette.primary.light,
-    },
-  }));
+const StyledButtonComponent = styled(Button)<StyledButtonProps>(({ theme }) => ({
+  backgroundColor: "transparent",
+  border: `1px solid ${theme.palette.primary.contrastText}`,
+  borderRadius: "3px",
+  padding: "5px 15px",
+  width: "100%",
+  color: theme.palette.primary.contrastText,
+  display: "inline-flex",
+  alignItems: "center",
+  justifyContent: "center",
+  gap: "10px",
+  textDecoration: "none", // Adiciona esta linha para remover o sublinhado
+  '&:hover': {
+    backgroundColor: theme.palette.primary.light,
+  },
+}));
 
+const StyledButton: React.FC<StyledButtonProps> = ({ children, component = "button", href, download }) => {
   return (
-    <StyledButtonComponent href={href} download={download}>
+    <StyledButtonComponent component={component} href={href} download={download}>
       {children}
     </StyledButtonComponent>
   );
-};
+}
 
 export default StyledButton;
